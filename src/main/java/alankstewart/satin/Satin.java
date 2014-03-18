f8a679d7bf4fad906b96459c79eb009ec9394fa0
Alan Stewart <alankstewart@gmail.com>
2014-03-19 08:47:59 +1100

    Modified using Java SE 8

--- src/main/java/alankstewart/satin/Satin.java
+++ src/main/java/alankstewart/satin/Satin.java
@@ -1,11 +1,13 @@
 /*
- * User: Alan K Stewart Date: 27/05/2002 Time: 06:20:51
+ * User: Alan K Stewart Date: 19/03/2014
  */
 
 package alankstewart.satin;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,38 +15,39 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Formatter;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.stream.Collectors;
 
 import static alankstewart.satin.Laser.CO2;
-import static java.lang.Float.parseFloat;
+import static java.lang.Double.parseDouble;
 import static java.lang.Integer.parseInt;
-import static java.lang.Math.PI;
-import static java.lang.Math.exp;
-import static java.lang.Math.pow;
+import static java.lang.Math.*;
 import static java.lang.System.nanoTime;
 import static java.lang.System.out;
 import static java.math.BigDecimal.ROUND_HALF_UP;
 import static java.math.BigDecimal.valueOf;
+import static java.nio.charset.Charset.defaultCharset;
+import static java.nio.file.StandardOpenOption.*;
 import static java.util.Collections.unmodifiableList;
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
 
 public final class Satin {
 
-    private static final float RAD = 0.18f;
-    private static final float W1 = 0.3f;
-    private static final float DR = 0.002f;
-    private static final float DZ = 0.04f;
-    private static final float LAMDA = 0.0106f;
+    private static final double RAD = 0.18;
+    private static final double W1 = 0.3;
+    private static final double DR = 0.002;
+    private static final double DZ = 0.04;
+    private static final double LAMDA = 0.0106;
     private static final double AREA = PI * pow(RAD, 2);
     private static final double Z1 = PI * pow(W1, 2) / LAMDA;
     private static final double Z12 = Z1 * Z1;
     private static final double EXPR = 2 * PI * DR;
     private static final int INCR = 8001;
+    private static final Path PATH = Paths.get(System.getProperty("user.dir"));
 
     public static void main(final String[] args) {
         final long start = nanoTime();
@@ -62,17 +65,17 @@ public final class Satin {
         }
     }
 
-    private void calculateConcurrently() throws IOException {
+    private void calculateConcurrently() throws IOException, URISyntaxException {
         final List<Integer> inputPowers = getInputPowers();
         final List<Laser> laserData = getLaserData();
 
-        final List<Callable<Void>> tasks = laserData.stream().map(laser -> new Callable<Void>() {
+        final List<Callable<Void>> tasks = laserData.parallelStream().map(laser -> new Callable<Void>() {
             @Override
             public Void call() throws Exception {
                 process(inputPowers, laser);
                 return null;
             }
-        }).collect(Collectors.toList());
+        }).collect(toList());
 
         final ExecutorService executorService = Executors.newCachedThreadPool();
         try {
@@ -86,7 +89,7 @@ public final class Satin {
         }
     }
 
-    private void calculate() throws IOException {
+    private void calculate() throws IOException, URISyntaxException {
         final List<Integer> inputPowers = getInputPowers();
         final List<Laser> laserData = getLaserData();
 
@@ -95,21 +98,27 @@ public final class Satin {
         }
     }
 
-    private List<Integer> getInputPowers() throws IOException {
-       return readDataFile("/pin.dat").stream().map(line -> parseInt(line)).collect(Collectors.toList());
+    private List<Integer> getInputPowers() throws IOException, URISyntaxException {
+        return Files.lines(getDataFilePath("pin.dat")).map(line -> parseInt(line)).collect(toList());
     }
 
-    private List<Laser> getLaserData() throws IOException {
-        return readDataFile("/laser.dat").stream().map(line -> line.split("  ")).map(gainMediumParams -> new Laser
-                (gainMediumParams[0], parseFloat(gainMediumParams[1]
-                .trim()), parseInt(gainMediumParams[2].trim()), CO2.valueOf(gainMediumParams[3].trim()))).collect
-                (Collectors.toList());
+    private List<Laser> getLaserData() throws IOException, URISyntaxException {
+        return Files.lines(getDataFilePath("laser.dat")).map(line -> line.split("  ")).map(gainMediumParams -> new Laser
+                (gainMediumParams[0], parseDouble(gainMediumParams[1]
+                        .trim()), parseInt(gainMediumParams[2].trim()), CO2.valueOf(gainMediumParams[3].trim()))).collect
+                (toList());
+    }
+
+    private Path getDataFilePath(String fileName) throws URISyntaxException {
+        final URL resource = getClass().getClassLoader().getResource(fileName);
+        requireNonNull(resource, "Failed to find resource " + fileName);
+        return Paths.get(resource.toURI());
     }
 
     private void process(final List<Integer> inputPowers, final Laser laser) throws IOException {
-        final Path path = Paths.get(System.getProperty("java.io.tmpdir"), laser.getOutputFile());
-        Files.deleteIfExists(path);
-        try (final Formatter formatter = new Formatter(Files.createFile(path).toFile())) {
+        final Path path = PATH.resolve(laser.getOutputFile());
+        try (BufferedWriter writer = Files.newBufferedWriter(path, defaultCharset(), CREATE, WRITE, TRUNCATE_EXISTING);
+             final Formatter formatter = new Formatter(writer)) {
             formatter
                     .format("Start date: %s\n\nGaussian Beam\n\nPressure in Main Discharge = %skPa\nSmall-signal Gain = %s\nCO2 via %s\n\nPin\t\tPout\t\tSat. Int.\tln(Pout/Pin)\tPout-Pin\n(watts)\t\t(watts)\t\t(watts/cm2)\t\t\t(watts)\n", Calendar
                             .getInstance().getTime(), laser.getDischargePressure(), laser.getSmallSignalGain(), laser
@@ -125,7 +134,7 @@ public final class Satin {
         }
     }
 
-    private List<Gaussian> gaussianCalculation(final int inputPower, final float smallSignalGain) {
+    private List<Gaussian> gaussianCalculation(final int inputPower, final double smallSignalGain) {
         final List<Gaussian> gaussians = new ArrayList<>();
 
         final double[] expr1 = new double[INCR];
@@ -140,7 +149,7 @@ public final class Satin {
         for (int saturationIntensity = 10000; saturationIntensity <= 25000; saturationIntensity += 1000) {
             double outputPower = 0.0;
             final double expr3 = saturationIntensity * expr2;
-            for (float r = 0; r <= 0.5f; r += DR) {
+            for (float r = 0; r <= 0.5; r += DR) {
                 double outputIntensity = inputIntensity * exp(-2 * pow(r, 2) / pow(RAD, 2));
                 for (int j = 0; j < INCR; j++) {
                     outputIntensity *= (1 + expr3 / (saturationIntensity + outputIntensity) - expr1[j]);
@@ -151,19 +160,5 @@ public final class Satin {
         }
 
         return unmodifiableList(gaussians);
-    }
-
-    private List<String> readDataFile(final String fileName) throws IOException {
-        final List<String> lines = new ArrayList<>();
-        try (final InputStream inputStream = getClass().getResourceAsStream(fileName);
-             final Scanner scanner = new Scanner(inputStream)) {
-            while (scanner.hasNext()) {
-                final String line = scanner.nextLine();
-                if (line != null && !line.isEmpty()) {
-                    lines.add(line);
-                }
-            }
-        }
-        return unmodifiableList(lines);
     }
 }
