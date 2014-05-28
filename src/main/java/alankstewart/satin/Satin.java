4d6871c23971b25e16a3b2c5f1efbebc58f15b6a
Alan Stewart <alankstewart@gmail.com>
2014-05-28 19:00:29 +1000

    Renamed variable

--- src/main/java/alankstewart/satin/Satin.java
+++ src/main/java/alankstewart/satin/Satin.java
@@ -99,9 +99,9 @@ public final class Satin {
     }
 
     private List<Laser> getLaserData() throws IOException, URISyntaxException {
-        final Pattern pattern = Pattern.compile("((md|pi)[a-z]{2}\\.out)\\s+([0-9]{2}\\.[0-9])\\s+([0-9]+)\\s+(?i:\\2)");
+        final Pattern p = Pattern.compile("((md|pi)[a-z]{2}\\.out)\\s+([0-9]{2}\\.[0-9])\\s+([0-9]+)\\s+(?i:\\2)");
         return lines(getDataFilePath("laser.dat"))
-                .map(pattern::matcher)
+                .map(p::matcher)
                 .filter(Matcher::matches)
                 .map(m -> new Laser(m.group(1), m.group(3), m.group(4), m.group(2)))
                 .collect(toList());
