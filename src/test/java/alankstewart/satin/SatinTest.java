597942cd56bccfe546075637ea3f23b9c2bd3b1a
Alan Stewart <alankstewart@gmail.com>
2018-12-22 20:32:43 +1100

    Formatting

--- src/test/java/alankstewart/satin/SatinTest.java
+++ src/test/java/alankstewart/satin/SatinTest.java
@@ -1,6 +1,5 @@
 package alankstewart.satin;
 
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
