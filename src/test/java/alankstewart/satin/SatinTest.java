221cf5f037cacd37f364a7a977cf772568d6ebcb
Alan Stewart <alankstewart@gmail.com>
2023-09-20 16:04:17 +1000

    Edit test

--- src/test/java/alankstewart/satin/SatinTest.java
+++ src/test/java/alankstewart/satin/SatinTest.java
@@ -1,8 +1,13 @@
 package alankstewart.satin;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -35,4 +40,12 @@ class SatinTest {
     private double roundUp(double value) {
         return Math.round(value * 1000.0) / 1000.0;
     }
+
+//    @Disabled
+//    @Test
+//    public void decodeBase64() throws IOException {
+//        var outputPath = Paths.get("D:\\temp", "mdcf-parent.zip");
+//        var path = Paths.get("D:\\temp", "mdcf-parent.txt");
+//        Files.write(outputPath, Base64.getDecoder().decode(Files.readAllBytes(path)));
+//    }
 }
