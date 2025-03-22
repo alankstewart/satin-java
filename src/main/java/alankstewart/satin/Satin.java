50b8e8656fe6d64e8d33e0d95d928c10be2dbf46
Alan Stewart <alankstewart@gmail.com>
2025-03-22 19:34:33 +1100

    inline creation of Gaussian

--- src/main/java/alankstewart/satin/Satin.java
+++ src/main/java/alankstewart/satin/Satin.java
@@ -130,13 +130,11 @@ public final class Satin {
     List<Gaussian> gaussianCalculation(final int inputPower, final double smallSignalGain) {
         return IntStream.iterate(10000, i -> i <= 25000, i -> i + 1000)
                 .parallel()
-                .mapToObj(saturationIntensity -> createGaussian(inputPower, smallSignalGain, saturationIntensity))
+                .mapToObj(saturationIntensity -> new Gaussian(
+                        inputPower,
+                        calculateOutputPower(inputPower, smallSignalGain, saturationIntensity),
+                        saturationIntensity))
                 .toList();
-    }
-
-    private Gaussian createGaussian(int inputPower, double smallSignalGain, int saturationIntensity) {
-        var outputPower = calculateOutputPower(inputPower, smallSignalGain, saturationIntensity);
-        return new Gaussian(inputPower, outputPower, saturationIntensity);
     }
 
     private double calculateOutputPower(int inputPower, double smallSignalGain, int saturationIntensity) {
