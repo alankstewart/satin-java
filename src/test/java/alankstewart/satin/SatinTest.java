3ff30f439c175df8f1b1ec93f03d3b66ee99e9d5
Alan Stewart <alankstewart@gmail.com>
2025-09-03 11:24:31 +1000

    Use virtual threads in test

--- src/test/java/alankstewart/satin/SatinTest.java
+++ src/test/java/alankstewart/satin/SatinTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,17 +22,31 @@ class SatinTest {
                                   int saturationIntensity,
                                   double outputPower,
                                   double logOutputPowerDividedByInputPower,
-                                  double outputPowerMinusInputPower) {
+                                  double outputPowerMinusInputPower) throws Exception {
+
         var satin = new Satin();
-        satin.gaussianCalculation(inputPower, smallSignalGain).parallelStream()
-                .filter(gaussian -> gaussian.saturationIntensity() == saturationIntensity)
-                .findAny()
-                .ifPresentOrElse(gaussian ->
-                        assertAll(
-                                () -> assertEquals(outputPower, roundUp(gaussian.outputPower())),
-                                () -> assertEquals(logOutputPowerDividedByInputPower, roundUp(gaussian.logOutputPowerDividedByInputPower())),
-                                () -> assertEquals(outputPowerMinusInputPower, roundUp(gaussian.outputPowerMinusInputPower()))
-                        ), Assertions::fail);
+
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            satin.gaussianCalculation(inputPower, smallSignalGain).stream()
+                    .map(gaussian -> executor.submit(() -> gaussian))
+                    .map(this::getGaussian)
+                    .filter(gaussian -> gaussian.saturationIntensity() == saturationIntensity)
+                    .findAny()
+                    .ifPresentOrElse(
+                            gaussian -> assertAll(
+                                    () -> assertEquals(outputPower, roundUp(gaussian.outputPower())),
+                                    () -> assertEquals(logOutputPowerDividedByInputPower, roundUp(gaussian.logOutputPowerDividedByInputPower())),
+                                    () -> assertEquals(outputPowerMinusInputPower, roundUp(gaussian.outputPowerMinusInputPower()))
+                            ), Assertions::fail);
+        }
+    }
+
+    private Satin.Gaussian getGaussian(Future<Satin.Gaussian> future) {
+        try {
+            return future.get();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private double roundUp(double value) {
